6d001176f7bfe4334c16d09cfcb63476b5be3e23
Silja K <silja.karesto@metropolia.fi>
2016-09-23 13:49:37 +0300

    Start.activity links to Main.activity
    Created button to link start activity to main activity and show list
    view in main activity

--- app/src/main/java/miasilja/androidapp/MainActivity.java
+++ app/src/main/java/miasilja/androidapp/MainActivity.java
@@ -1,52 +1,38 @@
 package miasilja.androidapp;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> pelitListaSisalto = new ArrayList<String>() {{
+        for (int i = 0; i < 5; i++) {
+            add("Ammattikorkeakoulu A - Yliopisto B");
+            add("Ammattikorkeakoulu B - Ammattikorkeakoulu C");
+            add("Yliopisto A - Yliopisto C");
+        }
+    }};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String nimi = intent.getStringExtra("EXTRA_MESSAGE");
+        TextView tervetuloaNimi = (TextView) findViewById(R.id.tervetuloa_nimi);
+        tervetuloaNimi.append(nimi);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        ListView lv = (ListView) findViewById(R.id.pelit_lista);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.single_game_item, R.id.single_game_item_view, pelitListaSisalto);
+        lv.setAdapter(listAdapter);
     }
 }
--- app/src/main/java/miasilja/androidapp/StartActivity.java
+++ app/src/main/java/miasilja/androidapp/StartActivity.java
@@ -1,7 +1,12 @@
 package miasilja.androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void siirraKayttaja(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edittext_nimi);
+        String nimi = editText.getText().toString();
+        intent.putExtra("EXTRA_MESSAGE", nimi);
+        startActivity(intent);
     }
 }
